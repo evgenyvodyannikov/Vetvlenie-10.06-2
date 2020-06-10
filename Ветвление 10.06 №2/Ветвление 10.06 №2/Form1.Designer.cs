829e48c0854a7ab4661a241f6415b8599b0a40c8
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-10 15:36:17 +0500

    форма и рисование

--- Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.Designer.cs
+++ Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(160, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 359);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.cs
+++ Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.cs
@@ -16,5 +16,23 @@ namespace Ветвление_10._06__2
         {
             InitializeComponent();
         }
+        Graphics g;
+        Bitmap bmp;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            g.FillRectangle(Brushes.Gray, 100, 0, 180, 280);
+            g.FillEllipse(Brushes.Red, 150, 10, 80, 80);
+            g.FillEllipse(Brushes.DarkGray, 150, 100, 80, 80);
+            g.FillEllipse(Brushes.DarkGray, 150, 190, 80, 80);
+            pictureBox1.Image = bmp;
+            pictureBox1.Invalidate();
+        }
     }
 }
