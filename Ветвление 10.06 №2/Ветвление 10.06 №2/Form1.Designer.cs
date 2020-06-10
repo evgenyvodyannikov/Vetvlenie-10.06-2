72105c7a0b4bc278a24f6c80e1ba8a0da9ccb4a1
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-10 16:11:15 +0500

    Конец.

--- Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.Designer.cs
+++ Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox1.Location = new System.Drawing.Point(160, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(272, 11);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(369, 359);
             this.pictureBox1.TabIndex = 0;
@@ -45,32 +47,66 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 176);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(126, 85);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 54);
+            this.button1.Size = new System.Drawing.Size(123, 54);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(30, 141);
+            this.textBox1.Location = new System.Drawing.Point(149, 41);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.BackgroundImage = global::Ветвление_10._06__2.Properties.Resources.q;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(12, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 54);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 50);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите время, прошедшее\r\nс начала часа:\r\n";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(653, 376);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Водянников 10.06 Задание 20";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -83,6 +119,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.cs
+++ Ветвление 10.06 №2/Ветвление 10.06 №2/Form1.cs
@@ -16,23 +16,76 @@ namespace Ветвление_10._06__2
         {
             InitializeComponent();
         }
-        Graphics g;
+        public Graphics g;
         Bitmap bmp;
         private void button1_Click(object sender, EventArgs e)
         {
-            
-        }
+            try
+            {
+                g.FillEllipse(Brushes.DarkGray, 150, 10, 80, 80);
+                g.FillEllipse(Brushes.DarkGray, 150, 100, 80, 80);
+                g.FillEllipse(Brushes.DarkGray, 150, 190, 80, 80);
+                int fulltime = 6;
+                float t = float.Parse(textBox1.Text);
+                double sk = t % fulltime;
+                if (sk > 0 & sk <= 3)
+                { g.FillEllipse(Brushes.Green, 150, 190, 80, 80); }
+                else if (sk > 3 & sk <= 4)
+                { g.FillEllipse(Brushes.Yellow, 150, 100, 80, 80); }
+                else { g.FillEllipse(Brushes.Red, 150, 10, 80, 80); }
+                pictureBox1.Invalidate();
+            }
+            catch
+            {
 
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(bmp);
-            g.FillRectangle(Brushes.Gray, 100, 0, 180, 280);
-            g.FillEllipse(Brushes.Red, 150, 10, 80, 80);
+            g.FillRectangle(Brushes.Gray, 100, 5, 180, 280);
+            g.FillEllipse(Brushes.DarkGray, 150, 10, 80, 80);
             g.FillEllipse(Brushes.DarkGray, 150, 100, 80, 80);
             g.FillEllipse(Brushes.DarkGray, 150, 190, 80, 80);
+            g.FillRectangle(Brushes.Black, 180, 285, 25, 90);
             pictureBox1.Image = bmp;
             pictureBox1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работа светофора для водителей запрограммирована следующим образом: в начале каждого часа в течение трех минут горит зеленый сигнал, затем в течение одной минуты — желтый, в течение двух минут — красный, в течение трех минут — опять зеленый и т. д. Дано вещественное число t, означающее время в минутах, прошедшее с начала очередного часа. Определить, сигнал какого цвета горит для водителей в этот момент.", "Задание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Int32.Parse(textBox1.Text) > 60)
+                    textBox1.Text = "60";
+            }
+            catch { }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                if (textBox1.Text.Length == 2 && textBox1.Text.Length <= 2)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Back)
+                {
+                    return;
+                }
+            }
+            e.Handled = true;
         }
     }
 }
